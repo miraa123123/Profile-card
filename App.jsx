a0828e4b6aca9e04b4466596859562f2174cdf41
miraa123123 <134292120+miraa123123@users.noreply.github.com>
2023-07-22 19:39:01 +0300

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,48 @@
+import React from "react"
+
+function App() {
+
+  return (
+    <>
+      <section className="main">
+        <div className="container">
+          <div className="banner"></div>
+          <div className="pfp"></div>
+
+            <div className="name">
+              <h1>Victor Crest</h1>
+              <h2>26</h2>
+            </div>
+
+            <div className="location">
+              <h3>London</h3>
+            </div>
+
+            <div className="bottem">
+              <div className="followers">
+                <h1>80k</h1>
+                <h3>Followers</h3>
+              </div>
+
+              <div className="likes">
+              <h1>803k</h1>
+              <h3>Likes</h3>
+              </div>
+
+              <div className="photos">
+              <h1>1.4k</h1>
+              <h3>Photos</h3>
+              </div>
+            </div>
+
+        </div>
+      </section>
+
+      <div className="end">
+        <h4>Made by mira azad</h4>
+      </div>
+    </>
+  )
+}
+
+export default App
